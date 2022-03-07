94eeb72e63e324c5f6ba4856db4b7eeba780732f
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-03-07 22:37:11 +0800

    add 1037

---
+++ 1036-1045/1037/1037.js
@@ -0,0 +1,28 @@
+//cat input.txt | node 1037.js
+var readline = require("readline");
+
+var lines = [];
+var rl = readline.createInterface({
+  input: process.stdin,
+});
+
+rl.on("line", function (line) {
+  lines.push(line);
+});
+
+rl.on("close", function () {
+  solve(lines);
+});
+
+function solve(lines) {
+  let target = lines[0];
+  let num = Number(lines[1]);
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(lines[i + 2]);
+  }
+  let ansArr = arr.filter((el) => el !== target);
+  ansArr.forEach((el) => {
+    console.log(el);
+  });
+}
---
+++ 1036-1045/1037/readme.md
@@ -0,0 +1,3 @@
+# arr.filter(el => el!==1) 陣列篩選元素
+
+https://oj.lidemy.com/problem/1037
