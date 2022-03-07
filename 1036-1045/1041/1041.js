b993e0a4e58dd75c775b161075662d983be3b61d
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-03-07 23:14:49 +0800

    add 1041

---
+++ 1036-1045/1041/1041.js
@@ -0,0 +1,19 @@
+//cat input.txt | node 1041.js
+var readline = require("readline");
+
+var lines = [];
+var rl = readline.createInterface({
+  input: process.stdin,
+});
+
+rl.on("line", function (line) {
+  lines.push(line);
+});
+
+rl.on("close", function () {
+  solve(lines);
+});
+
+function solve(lines) {
+  console.log(lines[0].trim());
+}
---
+++ 1036-1045/1041/readme.md
@@ -0,0 +1,3 @@
+# String trim 去除空格
+
+https://oj.lidemy.com/problem/1041
